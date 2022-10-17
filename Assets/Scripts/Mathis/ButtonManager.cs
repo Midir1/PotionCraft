0785953f3284a4a61395d94c26cddaa7df1cd909
Layloro <layloro@gmail.com>
2022-10-17 15:55:29 +0200

    Change scene name

--- Assets/Scripts/Mathis/ButtonManager.cs
+++ Assets/Scripts/Mathis/ButtonManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
@@ -33,7 +34,7 @@ public class ButtonManager : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
             if (hit.collider && hit.collider.gameObject.name.Contains("part"))
             {
-                Debug.Log(hit.collider.gameObject.transform.parent.name);
+
                 char lastChar = hit.collider.gameObject.transform.parent.name[hit.collider.gameObject.transform.parent.name.Length - 1];
                 nb = lastChar - '0';
                 pickedCustomer = customerTab[nb - 1];
@@ -44,36 +45,37 @@ public class ButtonManager : MonoBehaviour
 
     public void test()
     {
-        //deplacement des clients
-        for (int i = 0; i < customerTab.Count; i++)
-        {
-            for (int j = 0; j < customerTab[i].nbPart; j++)
-            {
-                //Destroy(customer.partDisplay[i]);
-                customerTab[i].pos[j].x += 3.5f;
-                customerTab[i].partDisplay[j].transform.position = new Vector3(customerTab[i].pos[j].x, customerTab[i].pos[j].y);
-            }
-        }
-        
-        //ajout et suppression
         if (customerTab.Count < 5)
         {
+                //deplacement des clients
+                for (int i = 0; i < customerTab.Count; i++)
+            {
+                for (int j = 0; j < customerTab[i].nbPart; j++)
+                {
+                    //Destroy(customer.partDisplay[i]);
+                    customerTab[i].pos[j].x += 3.5f;
+                    customerTab[i].partDisplay[j].transform.position = new Vector3(customerTab[i].pos[j].x, customerTab[i].pos[j].y);
+                }
+            }
+        
+            //ajout et suppression
+        
             customer = new CustomerClass();
             customerTab.Add(customer);
             customer.DiplayCustomer(customerTab.Count);
             
         }
-        if (pickedCustomer != null)
-        {
-            {
-                for (int i = 0; i < customerTab[nb].nbPart; i++)
-                {
-                    Destroy(customerTab[nb].partDisplay[i]);
-                }
-                //Debug.Log(customerTab[0].Paiement());
-                customerTab.RemoveAt(nb);
-            }
-        }
+        //if ()
+        //{
+        //    Debug.Log(pickedCustomer.partDisplay[0].transform.parent.name);
+        //    for (int i = 0; i < customerTab[nb].nbPart; i++)
+        //    {
+        //        Destroy(customerTab[nb].partDisplay[i]);
+        //    }
+        //    //Debug.Log(customerTab[0].Paiement());
+        //    customerTab.RemoveAt(nb);
+            
+        //}
         
     }
 }
