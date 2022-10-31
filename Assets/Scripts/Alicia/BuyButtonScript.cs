8d8983fed3117b64df98f049b4163ae9050a0010
alicia-Aesma <alicia.nathalie17@gmail.com>
2022-10-31 12:26:45 +0100

    script

--- Assets/Scripts/Alicia/BuyButtonScript.cs
+++ Assets/Scripts/Alicia/BuyButtonScript.cs
@@ -15,10 +15,44 @@ public class BuyButtonScript : MonoBehaviour
     {
         if (currentItemShop !=null)
         {
-            Debug.Log("item select");
+            Debug.Log("item select : " + currentItemShop.Id);
             if(currentItemShop.BuyIt())
             {
-
+                Debug.Log("achat");
+                
+                switch(currentItemShop.Id)
+                {
+                    case "cauldron2":
+                        GameManager.Instance.cauldron[1].CauldronIsAvailable();
+                        break;
+                    case "cauldron3":
+                        GameManager.Instance.cauldron[2].CauldronIsAvailable();
+                        break;
+                    case "cauldron1_up1":
+                        GameManager.Instance.cauldron[0].SpeedUpgradeIsAvailable();
+                        break;
+                    case "cauldron2_up1":
+                        GameManager.Instance.cauldron[1].SpeedUpgradeIsAvailable();
+                        break;
+                    case "cauldron3_up1":
+                        GameManager.Instance.cauldron[2].SpeedUpgradeIsAvailable();
+                        break;
+                    case "cauldron1_up2":
+                        GameManager.Instance.cauldron[0].TimeUpgradeIsAvailable();
+                        break;
+                    case "cauldron2_up2":
+                        GameManager.Instance.cauldron[1].TimeUpgradeIsAvailable();
+                        break;
+                    case "cauldron3_up2":
+                        GameManager.Instance.cauldron[2].TimeUpgradeIsAvailable();
+                        break;
+                    case "tipBox":
+                        GameManager.Instance.tipBox.TipIsAvailable();
+                        break;
+                    default :
+                        Debug.Log("unknow ItemShop.Id");
+                        break;
+                }
             }
         }
         else
@@ -27,14 +61,6 @@ public class BuyButtonScript : MonoBehaviour
         }
     }
 
-        void Start()
-    {
-        
-    }
+  
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Attributes
+
+    [SerializeField] uint maxMoney = 999999;
+    [SerializeField] uint currentMoney = 1000;
+    public CauldronScript[] cauldron = {new CauldronScript(), new CauldronScript(), new CauldronScript()};
+    public TipBoxScript tipBox = new TipBoxScript();
+
+    #endregion
+
+    #region getters/setters
+    public uint CurrentMoney { get => currentMoney; }
+    public uint MaxMoney { get => maxMoney; }
+
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject go = new GameObject("@GameManager");
+                DontDestroyOnLoad(go);
+                instance = go.AddComponent<GameManager>();
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    /// <summary>
+    /// money to add to wallet
+    /// </summary>
+    /// <param name="money">money to add to wallet, absolute value</param>
+    public void AddMoney(uint money)
+    {
+        currentMoney += money;
+        if (currentMoney > maxMoney)
+            currentMoney = maxMoney;
+
+    }
+    /// <summary>
+    /// money to withdraw from wallet
+    /// </summary>
+    /// <param name="money">money to withdraw from wallet, absolute value</param>
+    public void RemoveMoney(uint money)
+    {
+        currentMoney -= money;
+        if (currentMoney < 0)
+            currentMoney = 0;
+
+    }
+}
