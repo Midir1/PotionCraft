866787cdba44ac2b208fd87e553f8da313655035
yazami <yazami.goto@gmail.com>
2022-10-31 12:31:34 +0100

    fini fini Rune paterne recognition

--- Assets/Scripts/Pierre/InputPaternRecognition.cs
+++ Assets/Scripts/Pierre/InputPaternRecognition.cs
@@ -14,6 +14,7 @@ public class InputPaternRecognition : MonoBehaviour
         NotInitialized,
         C,
         Z,
+        B,
         PaternMax,
     }
     [SerializeField] Patern patern = Patern.NotInitialized;
@@ -24,15 +25,21 @@ public class InputPaternRecognition : MonoBehaviour
 
     Vector2[] pathToCopy;
 
+
+    bool flag = false;
     bool flagTouchBegan = false;
-    
+    //Ptc Point To Check
+    [SerializeField] int numPtc;
+     int midlePtc;
     List<Object> arrPoint = new List<Object>();
     [SerializeField] float radiusCollision;
-     bool flag = false;
+
     List<Object> arrFingerPath = new List<Object>();
     float actualDistBTWpoint;
     [SerializeField] float distBTWpoint;
     Vector2 lastPointPos;
+
+    [SerializeField] float angle;
 
 
     void Start()
@@ -45,6 +52,9 @@ public class InputPaternRecognition : MonoBehaviour
                 break;
             case Patern.Z:
                 pathToCopy = new Vector2[] { new Vector2(-0.7097762f, 2.247475f), new Vector2(-0.006312944f, 2.247475f), new Vector2(0.7151877f, 2.265512f), new Vector2(1.418651f, 2.28355f), new Vector2(0.8234127f, 1.778499f), new Vector2(0.3183625f, 1.399712f), new Vector2(-0.2227633f, 0.9668112f), new Vector2(-0.7278138f, 0.4617608f), new Vector2(-1.232864f, -0.04328966f), new Vector2(-0.5474386f, 0.08297265f), new Vector2(0.1560246f, 0.1370853f), new Vector2(0.8594878f, 0.1370853f), new Vector2(1.599026f, 0.1370853f) };
+                break;
+            case Patern.B:
+                pathToCopy = new Vector2[] { new Vector2(-0.9934571f, -2.940771f), new Vector2(-1.010675f, -2.561983f), new Vector2(-1.010675f, -2.183195f), new Vector2(-0.9934571f, -1.821625f), new Vector2(-0.9762397f, -1.425619f), new Vector2(-0.959022f, -1.06405f), new Vector2(-0.9418043f, -0.6852615f), new Vector2(-0.9073691f, -0.2892557f), new Vector2(-0.8901514f, 0.08953214f), new Vector2(-0.8729339f, 0.4683203f), new Vector2(-0.8557162f, 0.8815426f), new Vector2(-0.8557162f, 1.260331f), new Vector2(-0.8557162f, 1.690772f), new Vector2(-0.8557162f, 2.017906f), new Vector2(-0.8384985f, 2.396694f), new Vector2(-0.821281f, 2.775482f), new Vector2(-0.821281f, 3.15427f), new Vector2(-0.5113637f, 2.896006f), new Vector2(-0.2530992f, 2.586088f), new Vector2(-0.01205233f, 2.327824f), new Vector2(0.2806474f, 2.052342f), new Vector2(0.5389119f, 1.77686f), new Vector2(0.7455235f, 1.432507f), new Vector2(0.5216942f, 1.139808f), new Vector2(0.1429065f, 0.915978f), new Vector2(-0.1497935f, 0.7093668f), new Vector2(-0.4769283f, 0.502755f), new Vector2(-0.8040633f, 0.3477961f), new Vector2(-0.5974517f, 0.02066135f), new Vector2(-0.3047521f, -0.2720383f), new Vector2(-0.08092292f, -0.5819553f), new Vector2(0.1429065f, -0.8746555f), new Vector2(0.3839533f, -1.167355f), new Vector2(0.6766528f, -1.442837f), new Vector2(0.3150826f, -1.632231f), new Vector2(0.03960051f, -1.890495f), new Vector2(-0.218664f, -2.165978f), new Vector2(-0.4941459f, -2.44146f), new Vector2(-0.7696282f, -2.716942f) };
                 break;
             case Patern.NotInitialized: Debug.LogWarning("Initialize patern"); break;
         }
@@ -64,6 +74,7 @@ public class InputPaternRecognition : MonoBehaviour
             if (touch.phase == TouchPhase.Began)
             {
                 flagTouchBegan = true;
+                midlePtc = (int)numPtc / 2;
                 //prepare for the new drawing
                 actualDistBTWpoint = distBTWpoint;
                 lastPointPos = fingerPos;
@@ -181,13 +192,22 @@ public class InputPaternRecognition : MonoBehaviour
                 actualDistBTWpoint = distBTWpoint - (actualDistBTWpoint - distFinger);
 
                 //2 - collision finger/ point
-                for (int i = 0; i < arrPoint.Count; i++)
+                //check only 3Points // Ptc For PointToCheck
+                int currentPtc = midlePtc;
+                for (int i = currentPtc - numPtc/2; i < currentPtc + numPtc/2; i++)
                 {
+                    if (i >= arrPoint.Count)
+                        break;
+
                     Object point = arrPoint[i];
                     Vector2 pointPos = point.GetComponent<Transform>().position;
                     float distColiision = Vector2.Distance(fingerPos, pointPos);
                     if (distColiision < radiusCollision)
                     {
+                        if (i > midlePtc)
+                        {
+                            midlePtc = i;
+                        }
                         //3 - angle fingerFoward / pointFoward
                         //dont chek engle if last point
                         if (arrPoint.Count <= i + 1)
@@ -196,8 +216,8 @@ public class InputPaternRecognition : MonoBehaviour
                             break;
                         }
                         Vector2 pointFoward = pointPos - (Vector2)arrPoint[i + 1].GetComponent<Transform>().position;
-                        float angle = Vector3.Angle(pointFoward, fingerFoward);
-                        if (angle < 30f)
+                        float anglePontFinger = Vector3.Angle(pointFoward, fingerFoward);
+                        if (anglePontFinger < angle)
                         {
                             flag = true;
                         }
