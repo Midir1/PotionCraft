889fdf5974d5beb34cfb3cd195065e1e1633e9b0
Midir1 <60351234+Midir1@users.noreply.github.com>
2022-11-14 16:59:11 +0100

    Continue Ingredients and Potions

--- Assets/Scripts/Jojo/TravailKillian/Inventory.cs
+++ Assets/Scripts/Jojo/TravailKillian/Inventory.cs
@@ -3,37 +3,36 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [HideInInspector] public int maxIngredient;
+    
     public List<Item> ingredients;
     
     [SerializeField] private List<Recipe> recipes;
-    public int maxIngredient = 2;
-
+    [SerializeField] private List<GameObject> potions;
+    [SerializeField] private Transform potionParent;
+    
     public void CraftPotion()
     {
-        foreach (var recipe in recipes)
+        for (var i = 0; i < recipes.Count; i++)
         {
+            var recipe = recipes[i];
+            
             for (var index = 0; index < recipe.ingredients.Count; index++)
             {
                 if (!ingredients.Contains(recipe.ingredients[index])) break;
 
-                if (index == ingredients.Count - 1 && recipe.ingredients.Contains(ingredients[index]))
+                if (index == recipe.ingredients.Count - 1 && ingredients.Contains(recipe.ingredients[index]))
                 {
+                    ingredients.Clear();
+                    //Instantiate(potions[i], potionParent);
+                    Instantiate(potions[0], potionParent);
+                    
                     Debug.Log(recipe.name);
                     return;
                 }
             }
-
-            //
-            // for (var index = 0; index < ingredients.Count; index++)
-            // {
-            //     if (!recipe.ingredients.Contains(ingredients[index])) break;
-            //     
-            //     if (index == ingredients.Count - 1 && recipe.ingredients.Contains(ingredients[index]))
-            //     {
-            //         Debug.Log(recipe.name);
-            //         return;
-            //     }
-            // }
         }
+
+        ingredients.Clear();
     }
 }
