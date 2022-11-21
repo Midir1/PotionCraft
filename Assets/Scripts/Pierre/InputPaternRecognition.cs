66bc8c8b00346a61a91a5011b0107b4cf2eb0f36
yazami <yazami.goto@gmail.com>
2022-11-21 15:23:41 +0100

    nouveau Input Patern Recognition

--- Assets/Scripts/Pierre/InputPaternRecognition.cs
+++ Assets/Scripts/Pierre/InputPaternRecognition.cs
@@ -10,42 +10,40 @@ using static UnityEngine.GraphicsBuffer;
 
 public class InputPaternRecognition : MonoBehaviour
 {
+    [SerializeField] Canvas canva;
+    [SerializeField] Object pointToInstantiate;
+    [SerializeField] Object lineToInstantiate;
+
     [SerializeField] RuneList runeList;
     [SerializeField] Patern patern = Patern.NotInitialized;
 
-    [SerializeField] Object lineToInstantiate;
     Object line;
     RuneDrawLine runeDrawLine;
-    [SerializeField] Object pointToInstantiate;
-    [SerializeField] Canvas canva;
 
-    Vector2[] baseRune;
-
-
-    bool flag = false;
-    bool flagTouchBegan = false;
     //Ptc Point To Check
     [SerializeField] int numPtc;
-     int midlePtc;
-    List<Object> arrPoint = new List<Object>();
     [SerializeField] float radiusCollision;
-
-    List<Object> arrFingerPath = new List<Object>();
-    float actualDistBTWpoint;
     [SerializeField] float distBTWpoint;
+    [SerializeField] float angleLimit;
+
+    float actualDistBTWpoint;
+
     Vector2 lastPointPos;
 
-    [SerializeField] float angle;
+    List<Object> arrPointRune = new List<Object>();
+    List<Vector2> arrPointLine = new List<Vector2>();
+    Vector2[] rune;
 
+    bool flag = false;
 
     void Start()
     {
-        baseRune = runeList.GetRune(patern);
-        
-        foreach (Vector2 pos in baseRune)
+        rune = runeList.GetRune(patern);
+
+        foreach (Vector2 pos in rune)
         {
             Object circleCopy = Instantiate(pointToInstantiate, pos, Quaternion.identity, canva.transform);
-            arrPoint.Add(circleCopy);
+            arrPointRune.Add(circleCopy);
         }
     }
 
@@ -58,146 +56,189 @@ public class InputPaternRecognition : MonoBehaviour
 
             if (touch.phase == TouchPhase.Began)
             {
-                flagTouchBegan = true;
-                midlePtc = (int)numPtc / 2;
 
-                line = Instantiate(lineToInstantiate);
-                runeDrawLine = line.GetComponent<RuneDrawLine>();
-
-                actualDistBTWpoint = distBTWpoint;
-                lastPointPos = fingerPos;
-
-                //Collision finger / starPoint
-                Vector2 startPointPos = arrPoint[0].GetComponent<Transform>().position;
-                float distColiision = Vector2.Distance(fingerPos, startPointPos);
-                if (!(distColiision < radiusCollision))
-                {
-                    runeDrawLine.Fail();
-                    ClearPath();
-                }
             }
-            if (flagTouchBegan)
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                DrawLine();
+            }
+            else if (touch.phase == TouchPhase.Ended)
             {
                 CheckIfOnPath();
             }
         }
     }
 
+
+
+    void DrawLine()
+    {
+        Touch touch = Input.GetTouch(0);
+        Vector2 fingerPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+        if (arrPointLine.Count == 0)
+        {
+            line = Instantiate(lineToInstantiate);
+            runeDrawLine = line.GetComponent<RuneDrawLine>();
+
+            arrPointLine.Add(fingerPos);
+            //ici on dessine
+            //runeDrawLine.newLineVertex(fingerPos);
+
+            actualDistBTWpoint = distBTWpoint;
+            lastPointPos = fingerPos;
+        }
+
+        float distFinger = Vector2.Distance(fingerPos, lastPointPos);
+        if (distFinger > actualDistBTWpoint)
+        {
+            flag = false;
+
+            arrPointLine.Add(fingerPos);
+            //ici on dessine
+            runeDrawLine.newLineVertex(fingerPos);
+
+            // Vector2 fingerFoward = lastPointPos - fingerPos;
+            lastPointPos = fingerPos;
+            //so that point are seperated by the same distance
+            actualDistBTWpoint = distBTWpoint - (actualDistBTWpoint - distFinger);
+        }
+    }
+
+    void CheckIfOnPath()
+    {
+        //premier point sur premier point
+        if (arrPointLine.Count <= 0)
+        {
+            return;
+        }
+        if (!(Vector2.Distance(arrPointLine[0], rune[0]) < radiusCollision))
+        {
+            Debug.Log("premier point return");
+            Failure();
+            return;
+        }
+
+        //Test in 2 part
+        //1 - collision finger/ point
+        //2 - angle fingerFoward / pointFoward
+        Touch touch = Input.GetTouch(0);
+        Vector2 fingerPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+        int furthestPointTutched = 0;
+        
+        for (int i = 0; i < arrPointLine.Count; i++)
+        {
+            //1 - collision finger/ point
+            //check only point around furthest point tutched
+            // Ptc For PointToCheck
+
+            int tempFPT = furthestPointTutched;
+            for (int ii = (tempFPT - numPtc); ii <= (tempFPT + numPtc); ii++)
+            {
+                if (ii < 0)
+                {
+                    Debug.Log("ii-");
+                    ii = 1;
+                }
+                else if (ii >= rune.Length)
+                {
+                    Debug.Log("ii+");
+                    break;
+                }
+
+                float distColiision = Vector2.Distance(arrPointLine[i], rune[ii]);
+
+                if (distColiision < radiusCollision)
+                {
+                    if (ii > furthestPointTutched)
+                    {
+                        furthestPointTutched = ii;
+                    }
+                    Debug.Log("test collision");
+                    //2 - angle fingerFoward / pointFoward
+                    //last Rune point
+                    if (ii >= rune.Length -1 )
+                    {
+                        Debug.Log("flag last Rune Point");
+                        flag = true;
+                        Success();
+                        return;
+                    }
+                    //first line point
+                    if (i < 1)
+                    {
+                        Debug.Log("flag First line point");
+                        flag = true;
+                        break;
+                    }
+
+                    Vector2 runeFoward = rune[ii] - rune[ii + 1];
+                    Vector2 LineFoward = arrPointLine[i - 1] - arrPointLine[i];
+                    float angleRuneLine = Vector3.Angle(runeFoward, LineFoward);
+                    if (angleRuneLine < angleLimit)
+                    {
+                        flag = true;
+                        Debug.Log("flag Angle");
+                    }
+                }
+            }
+            //you fail
+            if (!flag)
+            {
+                Debug.Log("end");
+                Failure();
+            }
+            flag = false;
+        }
+        //you fail if you dont win
+        Debug.Log("you fail if you dont win");
+        Failure();
+    }
+
+
     void ClearPath()
     {
         //clear the last path
         line = null;
-        arrPoint.ForEach(Destroy);
-        arrPoint.Clear();
-        arrFingerPath.ForEach(Destroy);
-        arrFingerPath.Clear();
+        arrPointRune.ForEach(Destroy);
+        arrPointRune.Clear();
+        arrPointLine.Clear();
         //recrate arrPoint 
-        foreach (Vector2 pos in baseRune)
+        foreach (Vector2 pos in rune)
         {
             Object circleCopy = Instantiate(pointToInstantiate, pos, Quaternion.identity, canva.transform);
-            arrPoint.Add(circleCopy);
-        }
-        flagTouchBegan = false;
-    }
-
-    void drawPoint(Vector2 _pos)
-    {
-        Object circleCopy = Instantiate(pointToInstantiate, _pos, Quaternion.identity, canva.transform);
-        UnityEngine.Color color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-        circleCopy.GetComponent<Image>().color = color;
-        arrFingerPath.Add(circleCopy);
-    }
-
-
-    void CheckIfOnPath()
-    {
-        //Test in 3 part
-        //1 - dist btw check
-        //2 - collision finger/ point
-        //3 - angle fingerFoward / pointFoward
-
-        Touch touch = Input.GetTouch(0);
-        Vector2 fingerPos = Camera.main.ScreenToWorldPoint(touch.position);
-        if (touch.phase == TouchPhase.Moved)
-        {
-            //1 - dist btw check
-            float distFinger = Vector2.Distance(fingerPos, lastPointPos);
-            if (distFinger > actualDistBTWpoint)
-            {
-                flag = false;
-
-                //ici on dessine
-                runeDrawLine.newLineVertex(fingerPos);
-
-                Vector2 fingerFoward = lastPointPos - fingerPos;
-                lastPointPos = fingerPos;
-                //so that point are seperated by the same distance
-                actualDistBTWpoint = distBTWpoint - (actualDistBTWpoint - distFinger);
-
-                //2 - collision finger/ point
-                //check only 3Points // Ptc For PointToCheck
-                int currentPtc = midlePtc;
-                for (int i = currentPtc - numPtc/2; i < currentPtc + numPtc/2; i++)
-                {
-                    if (i >= arrPoint.Count)
-                        break;
-
-                    Object point = arrPoint[i];
-                    Vector2 pointPos = point.GetComponent<Transform>().position;
-                    float distColiision = Vector2.Distance(fingerPos, pointPos);
-                    if (distColiision < radiusCollision)
-                    {
-                        if (i > midlePtc)
-                        {
-                            midlePtc = i;
-                        }
-                        //3 - angle fingerFoward / pointFoward
-                        //dont chek engle if last point
-                        if (arrPoint.Count <= i + 1)
-                        {
-                            flag = true;
-                            break;
-                        }
-                        Vector2 pointFoward = pointPos - (Vector2)arrPoint[i + 1].GetComponent<Transform>().position;
-                        float anglePontFinger = Vector3.Angle(pointFoward, fingerFoward);
-                        if (anglePontFinger < angle)
-                        {
-                            flag = true;
-                        }
-                    }
-                }
-                if (!flag)
-                {
-                    runeDrawLine.Fail();
-                    ClearPath();
-                }
-            }
-        }
-
-
-        if (touch.phase == TouchPhase.Ended)
-        {
-            //Collision finger / endPoint
-            Vector2 endPointPos = arrPoint[arrPoint.Count-1].GetComponent<Transform>().position;
-            float distColiision = Vector2.Distance(fingerPos, endPointPos);
-            if (distColiision < radiusCollision)
-            {
-                runeDrawLine.Braval();
-                ClearPath();
-            }
-            else
-            {
-                runeDrawLine.Fail();
-                ClearPath();
-            }
+            arrPointRune.Add(circleCopy);
         }
     }
-
 
 
     void SetPatern(Patern _patern)
     {
         patern = _patern;
+
+        arrPointRune.ForEach(Destroy);
+        arrPointRune.Clear();
+
+        rune = runeList.GetRune(patern);
+
+        foreach (Vector2 pos in rune)
+        {
+            Object circleCopy = Instantiate(pointToInstantiate, pos, Quaternion.identity, canva.transform);
+            arrPointRune.Add(circleCopy);
+        }
+    }
+
+    void Success()
+    {
+        runeDrawLine.Braval();
+        ClearPath();
+    }
+
+    void Failure()
+    {
+        runeDrawLine.Fail();
+        ClearPath();
     }
 
 
