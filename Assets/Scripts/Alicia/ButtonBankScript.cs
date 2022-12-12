b5c548984bf678a7146220d84ce7b95b597a7054
Yazamoule <yazami.goto@gmail.com>
2022-12-12 06:57:41 +0100

    Sond Shop

--- Assets/Scripts/Alicia/ButtonBankScript.cs
+++ Assets/Scripts/Alicia/ButtonBankScript.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ButtonBankScript : MonoBehaviour
 {
+    public UnityEvent ButonBank;
+    private void Start()
+    {
+        ButonBank = new UnityEvent();
+    }
     void OnMouseDown()
     {
+        ButonBank.Invoke();
         GameManager.Instance.shopState = ShopState.BANK;
     }
 }
--- Assets/Scripts/Alicia/ButtonShopScript.cs
+++ Assets/Scripts/Alicia/ButtonShopScript.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ButtonShopScript : MonoBehaviour
 {
+    public UnityEvent ButonShop;
+    private void Start()
+    {
+        ButonShop = new UnityEvent();
+    }
+
     void OnMouseDown()
     {
+        ButonShop.Invoke();
         GameManager.Instance.shopState = ShopState.SHOP;
     }
 }
